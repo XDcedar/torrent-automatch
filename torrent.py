b855fcb603d7e37b3ae2bb7e392ebd3a44f4ba32
XDCedar <xdcedar@foxmail.com>
2022-05-15 15:42:29 +0800

    formatted with black

--- torrent.py
+++ torrent.py
@@ -9,6 +9,7 @@ from hashlib import sha1
 import os
 import random
 
+
 @dataclass
 class BlockMeta:
     id: int = 0
@@ -20,6 +21,7 @@ class BlockMeta:
     first_byte_in_file: int = 0
     last_byte_in_file: int = 0
 
+
 @dataclass
 class FileMeta:
     path: str
@@ -29,6 +31,7 @@ class FileMeta:
     blocks: list
     match_candidates: list
     matches: list
+
     def __init__(self):
         self.path = ""
         self.length = 0
@@ -38,20 +41,28 @@ class FileMeta:
         self.match_candidates = []
         self.matches = []
 
+
 @dataclass
 class ExistedFileMeta:
     path: str = ""
     length: int = 0
 
+
 def do_arg_parse():
-    parser = argparse.ArgumentParser(description='“浮肿一时爽，一直浮肿一直爽。”')
-    parser.add_argument('--src-list', type=str, dest='src_list', help='specify a textfile, each line contains a file path of existing file as the potential linking source.')
-    parser.add_argument('--torrent', type=str, dest='torrent', help='the torrent file to work with')
-    parser.add_argument('--dst', type=str, dest='dst', help='the destination directory')
-    parser.add_argument('--blocks-to-check', type=int, dest='blocks_to_check', default=10)
-    parser.add_argument('--create-symlinks', dest='create_symlinks', action='store_true')
+    parser = argparse.ArgumentParser(description="“浮肿一时爽，一直浮肿一直爽。”")
+    parser.add_argument(
+        "--src-list",
+        type=str,
+        dest="src_list",
+        help="specify a textfile, each line contains a file path of existing file as the potential linking source.",
+    )
+    parser.add_argument("--torrent", type=str, dest="torrent", help="the torrent file to work with")
+    parser.add_argument("--dst", type=str, dest="dst", help="the destination directory")
+    parser.add_argument("--blocks-to-check", type=int, dest="blocks_to_check", default=10)
+    parser.add_argument("--create-symlinks", dest="create_symlinks", action="store_true")
     args = parser.parse_args()
     return args, parser
+
 
 def parse_files_meta(root_directory_path, files_info, block_hashes, block_size):
     file_metas = []
@@ -63,9 +74,9 @@ def parse_files_meta(root_directory_path, files_info, block_hashes, block_size):
     for f in files_info:
         fm = FileMeta()
         fm.path = root_directory_path
-        for p in f['path']:
+        for p in f["path"]:
             fm.path = os.path.join(fm.path, p)
-        fm.length = int(f['length'])
+        fm.length = int(f["length"])
         fm.first_byte = current_byte
         fm.last_byte = current_byte + fm.length
         fm.blocks = []
@@ -96,6 +107,7 @@ def parse_files_meta(root_directory_path, files_info, block_hashes, block_size):
             break
     return file_metas
 
+
 def parse_existed_files_meta(file_list):
     res = []
     for line in file_list:
@@ -104,6 +116,7 @@ def parse_existed_files_meta(file_list):
         efm.length = os.stat(efm.path).st_size
         res.append(efm)
     return res
+
 
 def pass1_check_identical(fm, efm, blocks_to_check):
     bms = fm.blocks[:]
@@ -116,7 +129,7 @@ def pass1_check_identical(fm, efm, blocks_to_check):
             break
     if len(sampled_bms) == 0:
         return False
-    ef = open(efm.path, 'rb')
+    ef = open(efm.path, "rb")
     identical = True
     for bm in sampled_bms:
         hasher = sha1()
@@ -129,6 +142,7 @@ def pass1_check_identical(fm, efm, blocks_to_check):
     ef.close()
     return identical
 
+
 # this function has side-effect!
 def pass2_check_identical(bm, file_metas):
     hasher = sha1()
@@ -139,14 +153,14 @@ def pass2_check_identical(bm, file_metas):
                 return False
             intersect_first_byte = max(fm.first_byte, bm.first_byte)
             intersect_last_byte = min(fm.last_byte, bm.last_byte)
-            #print(intersect_first_byte, intersect_last_byte)
+            # print(intersect_first_byte, intersect_last_byte)
             if intersect_first_byte == fm.first_byte and intersect_last_byte == fm.last_byte:
                 fm_covered.append(fm)
             intersect_first_byte_in_file = intersect_first_byte - fm.first_byte
             intersect_last_byte_in_file = intersect_last_byte - fm.first_byte
             # TODO: should emumerate over all possible combinations
             efm = fm.match_candidates[0]
-            ef = open(efm.path, 'rb')
+            ef = open(efm.path, "rb")
             ef.seek(intersect_first_byte_in_file)
             hasher.update(ef.read(intersect_last_byte_in_file - intersect_first_byte_in_file))
             ef.close()
@@ -157,6 +171,7 @@ def pass2_check_identical(bm, file_metas):
                 fm.matches.append(fm.match_candidates[0])
         return True
     return False
+
 
 if __name__ == "__main__":
     args, parser = do_arg_parse()
@@ -172,8 +187,10 @@ if __name__ == "__main__":
 
     try:
         data = torrent_parser.parse_torrent_file(args.torrent)
-        block_size = int(data['info']['piece length'])
-        file_metas = parse_files_meta(os.path.join(abs_dst, data['info']['name']), data['info']['files'], data['info']['pieces'], block_size)
+        block_size = int(data["info"]["piece length"])
+        file_metas = parse_files_meta(
+            os.path.join(abs_dst, data["info"]["name"]), data["info"]["files"], data["info"]["pieces"], block_size
+        )
     except Exception as e:
         print(e, file=sys.stderr)
         print("Failed to read the torrent file, or it is not a multiple-file torrent!", file=sys.stderr)
@@ -181,7 +198,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        file_list = open(args.src_list, "r", encoding='utf8')
+        file_list = open(args.src_list, "r", encoding="utf8")
         existed_file_metas = parse_existed_files_meta(file_list)
     except Exception as e:
         print(e, file=sys.stderr)
@@ -215,8 +232,12 @@ if __name__ == "__main__":
 
         # also, it may be the case where a file is covered by two blocks
         for fm in file_metas:
-            if len(fm.matches) == 0 and len(fm.blocks) == 2 and pass2_check_identical(fm.blocks[0], file_metas) \
-                and pass2_check_identical(fm.blocks[1], file_metas):
+            if (
+                len(fm.matches) == 0
+                and len(fm.blocks) == 2
+                and pass2_check_identical(fm.blocks[0], file_metas)
+                and pass2_check_identical(fm.blocks[1], file_metas)
+            ):
                 fm.matches.append(fm.match_candidates[0])
 
         """
@@ -230,7 +251,6 @@ if __name__ == "__main__":
         print("Failed to read some data blocks!", file=sys.stderr)
         parser.print_help()
         sys.exit(1)
-
 
     num_linked = 0
     try:
